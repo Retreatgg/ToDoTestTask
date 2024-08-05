ea04080a0bcb3ae37bd658bd2195652271424734
Ashimov Argen <retreatpm@icloud.com>
2024-08-05 13:40:55 +0300

    Add valid handle

--- src/main/java/com/example/todo/dtos/RegisterUserDto.java
+++ src/main/java/com/example/todo/dtos/RegisterUserDto.java
@@ -1,10 +1,21 @@
 package com.example.todo.dtos;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class RegisterUserDto {
+    @NotEmpty
     private String username;
+    @NotEmpty
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 4, max = 24, message = "Length must be >= 4 and <= 24")
+    @Pattern(
+            regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).+$",
+            message = "Should contain at least one UPPER case letter, one number"
+    )
     private String password;
 }
--- src/main/java/com/example/todo/exceptions/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/example/todo/exceptions/handlers/GlobalExceptionHandler.java
@@ -5,11 +5,17 @@ import com.example.todo.exceptions.AuthenticationException;
 import com.example.todo.exceptions.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<AppError> handleAuthenticationEx(AuthenticationException exception) {
@@ -19,5 +25,17 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<AppError> handleUserNotFoundEx(UserNotFoundException exception) {
         return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(), exception.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.badRequest().body(errors);
     }
 }
