1b06bf091170f6c6a48762a7420764a443e7cafe
Ashimov Argen <retreatpm@icloud.com>
2024-08-17 18:47:27 +0300

    Change text for status

--- src/main/java/com/example/todo/enums/Status.java
+++ src/main/java/com/example/todo/enums/Status.java
@@ -7,16 +7,16 @@ import java.util.NoSuchElementException;
 @Getter
 @RequiredArgsConstructor
 public enum Status {
-    NEW("НОВЫЙ"), PENDING("В ОЖИДАНИИ"), IN_PROGRESS("В ПРОГРЕССЕ"), DONE("СДЕЛАНА");
+    NEW("NEW"), PENDING("PENDING"), IN_PROGRESS("IN PROGRESS"), DONE("DONE");
 
     private final String status;
 
     public static Status getStatusEnum(String status) {
         return switch (status.toUpperCase()) {
-            case "НОВЫЙ" -> NEW;
-            case "В ПРОГРЕССЕ" -> IN_PROGRESS;
-            case "СДЕЛАНА" -> DONE;
-            case "В ОЖИДАНИИ" -> PENDING;
+            case "NEW" -> NEW;
+            case "IN PROGRESS" -> IN_PROGRESS;
+            case "DONE" -> DONE;
+            case "PENDING" -> PENDING;
             default -> throw new NoSuchElementException(String.format("No such status: '%s'", status));
         };
     }
