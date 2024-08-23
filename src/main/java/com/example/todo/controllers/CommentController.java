4e3c6a784f0dfb57315de06911bfd1594aa3affe
Ashimov Argen <retreatpm@icloud.com>
2024-08-23 13:27:34 +0300

    delete comments

--- src/main/java/com/example/todo/controllers/CommentController.java
+++ src/main/java/com/example/todo/controllers/CommentController.java
@@ -1,32 +1,32 @@
-package com.example.todo.controllers;
-
-import com.example.todo.dtos.CommentCreateDto;
-import com.example.todo.dtos.CommentDto;
-import com.example.todo.models.Task;
-import com.example.todo.services.CommentService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/tasks/{taskId}/comments")
-public class CommentController {
-
-    private final CommentService commentService;
-
-    @PostMapping
-    public HttpStatus create(@PathVariable Task taskId, @RequestBody @Valid CommentCreateDto createDto) {
-        commentService.create(createDto, taskId);
-        return HttpStatus.OK;
-    }
-
-    @GetMapping
-    public ResponseEntity<List<CommentDto>> getComments(@PathVariable Long taskId) {
-        return ResponseEntity.ok(commentService.getCommentsByTaskId(taskId));
-    }
-}
+//package com.example.todo.controllers;
+//
+//import com.example.todo.dtos.CommentCreateDto;
+//import com.example.todo.dtos.CommentDto;
+//import com.example.todo.models.Task;
+//import com.example.todo.services.CommentService;
+//import jakarta.validation.Valid;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/api/tasks/{taskId}/comments")
+//public class CommentController {
+//
+//    private final CommentService commentService;
+//
+//    @PostMapping
+//    public HttpStatus create(@PathVariable Task taskId, @RequestBody @Valid CommentCreateDto createDto) {
+//        commentService.create(createDto, taskId);
+//        return HttpStatus.OK;
+//    }
+//
+//    @GetMapping
+//    public ResponseEntity<List<CommentDto>> getComments(@PathVariable Long taskId) {
+//        return ResponseEntity.ok(commentService.getCommentsByTaskId(taskId));
+//    }
+//}
